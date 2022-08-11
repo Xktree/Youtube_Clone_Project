a2d44bd5d6740afee708fe545a72af6d5d041441
ali <aliyavuz2000@hotmail.com>
2022-08-11 18:01:50 -0400

    Models, urls and admin

--- backend/apps/replies/models.py
+++ backend/apps/replies/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from authentication.models import User
+from apps.comments.models import Comment
 
 # Create your models here.
 
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_type')
-    comment = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reply_comment_type')
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='reply_comment_type')
     text = models.CharField(max_length=255)

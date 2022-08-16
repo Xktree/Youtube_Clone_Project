012b320cb48b41a4107938979317e96f409d1ded
ali <aliyavuz2000@hotmail.com>
2022-08-16 17:09:57 -0400

    fixing merges

--- frontend/src/pages/HomePage/YoutubeHomePage.js
+++ frontend/src/pages/HomePage/YoutubeHomePage.js
@@ -1,39 +1,36 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import useAuth from "../../hooks/useAuth"; 
-import axios from 'axios';
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
 
 const YoutubeHomePage = () => {
+  const [user, token] = useAuth();
+  const [videos, setVideos] = useState([]);
 
-    const [user, token] = useAuth();
-    const [videos, setVideos] = useState([]);
-  
-    useEffect(() => {
-      const fetchVideos = async () => {
-        try {
-          let response = await axios.get("http://127.0.0.1:8000/api/login/", {
-            headers: {
-              Authorization: "Bearer " + token,
-            },
-          });
-          setVideos(response.data);
-        } catch (error) {
-          console.log(error.response.data);
-        }
-      };
-      fetchVideos();
-    }, [token]);
-    return (
-      <div className="container">
-        <h1>Home Page for {user.username}!</h1>
-        {videos &&
-          videos.map((video) => (
-            <p key={video.id}>
-              {/* {car.year} {car.model} {car.make} */}
-            </p>
-          ))}
-      </div>
-    );
-  };
-  
-  export default YoutubeHomePage;
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        let response = await axios.get("http://127.0.0.1:8000/api/login/", {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        });
+        setVideos(response.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+    fetchVideos();
+  }, [token]);
+  return (
+    <div className="container">
+      <h1>Home Page for {user.username}!</h1>
+      {videos &&
+        videos.map((video) => (
+          <p key={video.id}>{/* {car.year} {car.model} {car.make} */}</p>
+        ))}
+    </div>
+  );
+};
+
+export default YoutubeHomePage;

1156b8897524cb0a7e4d4b0533c3f9a749f3d647
jonathan sanchez <sanchezjon480@gmail.com>
2022-08-16 13:06:19 -0700

    Created SearchBar.jsx and updated with usestate

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const SearchBar = () => {
+
+    const [searchValue, setSearchValue] = useState ("")
+
+    return ( 
+       <form></form>
+     );
+}
+ 
+export default SearchBar;

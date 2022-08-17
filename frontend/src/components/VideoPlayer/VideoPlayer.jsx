e9684f96b041b064cbdf0d44a27450b08f432f23
jonathan sanchez <sanchezjon480@gmail.com>
2022-08-17 13:34:20 -0700

    updated videoplayer.jsx to reflect iframe component

---
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Video = ({videoId}) => {
+    return ( 
+        <>
+        <iframe
+        title='Youtube video'
+        id='ytplayer'
+        type='text/html'
+        width='640'
+        height='360'
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=0`}
+        frameBorder='0'
+        />
+        </>
+     );
+}
+ 
+export default Video;
